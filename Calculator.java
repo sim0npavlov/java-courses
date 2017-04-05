1abb29ff6bf8b2b3bf97d0549bec6cd4ba04fbdb
sim0npavlov <semyen.ps@gmail.com>
2017-04-05 13:08:35 +0500

    input user

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,86 @@
 public class Calculator {
 
-private int result;
-    public void add(int...params){
-        for (Integer param:params){
+private double first;
+private double second;
+private char operation;
+private double result;
+    
+    public void setFirst(double f) {
+        this.first = f;
+    }
+    public double getFirst() {
+        return first;
+    }
+    
+    public void setSecond (double s)  {
+        this.second = s;
+    }
+    public double getSecond() {
+        return second;
+    }
+    
+    public void setOperation (char o) {
+        this.operation = o;
+    }
+    public char getOperation () {
+        return operation;
+    }
+    
+    public void add(double...params){
+        for (Double param:params){
             this.result += param;
         }
+    
     }
-    public int getResult() {
+    
+    public void subtract(double f, double s) {
+        this.result = f - s;
+    }
+    
+    public void multiply(double f, double s) {
+        this.result = f * s;
+    }
+    
+    public void divide(double f, double s) {
+        
+    }
+    
+    public void pow(double f, double s) {
+        this.result = Math.pow(f, s);
+    }
+    
+    public double getResult() {
         return this.result;
     }
     public void cleanResult() {
         this.result = 0;
+    }
+    
+    public double calculate(double f, double s, char o) {
+        double res = 0;
+        switch(o) {
+            case '+':
+                add(f, s);
+                res = getResult ();
+                break;
+            case '-':
+                subtract(f, s);
+                res = getResult ();
+                break;
+            case '*':
+                multiply(f, s);
+                res = getResult ();
+                break;
+            case '/':
+                divide(f, s);
+                res = getResult ();
+                break;
+            case '^':
+                pow(f, s);
+                res = getResult ();
+                break;
+        }
+      return res;    
     }
 
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,21 +1,43 @@
-import java.util.Scanner;
+import java.util.Scanner; // Импортируем класс
 
-public class InteractRunner {
+public class InteractRunner { 
     
     public static void main(String[] arg) {
-        Scanner reader = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in); // Создаем обьект класса Skaner
         try {
             Calculator calc = new Calculator ();
-            String exit = "n";
+            String exit = "n"; 
+            String clean = "y";     // Объявили переменные
+            double res = 0;
+            
             while (!exit.equals("y")){
-                System.out.println("Enter first arg : ");
-                String first = reader.next();
-                System.out.println ("Ener second arg : ");
-                String second = reader.next();
-                calc.add(Integer.valueOf(first), Integer.valueOf(second));
-                System.out.println ("Result : " + calc.getResult());
+                if (clean.equals ("n")){
+                    
+                    calc.setFirst(res); // задать res
+                    System.out.println("First arg : " + calc.getFirst()); // вывод первого аргумента
+                    calc.cleanResult();
+                } else {
+                    calc.cleanResult ();
+                       System.out.println ("Enter first arg : "); //Ввели первый аргумент
+                       calc.setFirst (reader.nextDouble());
+                    }
+        
+                System.out.println ("Ener second arg : "); // второй аргумент
+                calc.setSecond(reader.nextDouble());
+                
+                System.out.println ("Enter operation for calc (+, -, *, /, ^) : "); // Ввели операцию
+                calc.setOperation(reader.next().charAt(0)); // Считал 0 символ из массива
+                
+                res = calc.calculate(calc.getFirst(), calc.getSecond(), calc.getOperation()); // присвоили значение результата
+                
+                System.out.println ("Result : " + res); // Вывели результат
+                
                 System.out.println ("Exit :  y/n ");
                 exit=reader.next();
+                if (exit.equals("yes")) break;
+                
+                System.out.println ("Clean result : y/n");
+                clean = reader.next();
             }
             
         }
